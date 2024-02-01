9099f3beb51b831cc7b83f49ddebc6d8e6d36bf1
Filip-Peev <Filip.Peev.Official@gmail.com>
2024-02-01 20:20:56 +0200

    added - JS Fundamentals - Lab

---
+++ JS Fundamentals - Lab/07. Smallest of Three Numbers.js
@@ -0,0 +1,6 @@
+function findSmallest(firstNumber, secondNumber, thirdNumber) {
+    'use strict';
+
+    const smallestNumber = Math.min(firstNumber, secondNumber, thirdNumber)
+    console.log(smallestNumber)
+}
--- JS Fundamentals - Lab/demo.js
+++
@@ -1,9 +0,0 @@
-function demo() {
-    let array = [1, 2, 3, "pesho"];
-
-    array[5] = "10";
-
-    console.log(array);
-}
-
-demo();
